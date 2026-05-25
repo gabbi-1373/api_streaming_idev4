e5aff0d885ce9965cdb061eaba826a5bbc0136b2
gabbi-1373 <emillygabrielly03082009@gmail.com>
2026-05-25 10:51:16 -0300

    codando

---
+++ routes/filmes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const mongoose = require ('mongoose');
+const router = express.Router();
+
+const musicasSchema = new mongoose.Schema ({
+    
+})
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -13,6 +13,18 @@ mongoose.connection.on('connected' , () => {
     console.log('Conectado com o MongoDB');
 });
 
+const MusicasRouter = require('./routes/musicas');
+const FilmesRouter = require('./routes/filmes');
+const SeriesRouter = require('./routes/series');
+const LivrosRouter = require('./routes/livros');
+const jogosRouter = require('./routes/jogos');
+
+app.use('./musicas', MusicasRouter);
+app.use('./filmes', FilmesRouter);
+app.use('./series', SeriesRouter);
+app.use('./livros', LivrosRouter);
+app.use('./jogos', jogosRouter);
+
 app.listen(3000, () => {
     console.log('Servidor rodando na porta 3000')
 });
