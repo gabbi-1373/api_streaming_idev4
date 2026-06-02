d06459f38e47e3f33035b16e00112cd909089e33
gabbi-1373 <emillygabrielly03082009@gmail.com>
2026-06-02 14:36:03 -0300

    musicas

--- routes/musicas.js
+++ routes/musicas.js
@@ -2,6 +2,58 @@ const express = require('express');
 const mongoose = require ('mongoose');
 const router = express.Router();
 
+const musicasSchema = new mongoose.Schema ({
+   titulo: String,
+    artista: String,
+    genero: String,
+    ano: Number,
+    duracao: Number,
+    disponivel: Boolean,
+    detalhes: Object
+});
 
+const Musica = mongoose.model('musicas', musicasSchema);
+
+router.get('/', async (req, res) => {
+    try {
+        const musicas = await Musica.find();
+        res.json(musicas);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const novaMusica = new Musica(req.body);
+        await novaMusica.save();
+        res.status(201).json(novaMusica);
+    } catch (error) {
+        res.status(400).json({
+            message: error.message,
+
+        });
+    }
+    });
+
+router.put('/:id', async (req, res) => {
+    try{
+        const musicaAtualizada = await Musica.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true }
+        );
+        if(!musicaAtualizada) {
+            return res.status(404).json({ 
+                message: 'Música não encontrada' 
+            });
+        }
+        res.status(200).json(musicaAtualizada);
+    } catch (error) {
+        res.status(400).json({ 
+            message: error.message 
+        });
+    }
+});
 
 module.exports = router;
